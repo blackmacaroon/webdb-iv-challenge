e2a30b537d104b57afe74350906daefc0963ba75
Kayla <6lackmacaroon@gmail.com>
2019-06-06 17:07:03 -0600

    added routes and handlers for ingredients and recipe names

--- api/routes/ingredientsRouter.js
+++ api/routes/ingredientsRouter.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const db = require('../../data/helpers/dishesDB.js');
+const db = require('../../data/helpers/ingredientsDB.js');
 
 router
       .get('/', (req, res) => {
             db.find()
-                  .then(dishes => {
-                        res.status(200).json(dishes);
+                  .then(ingredients => {
+                        res.status(200).json(ingredients);
                   })
                   .catch(err => {
                         res.status(500).json({ error: 'could not get anything from the database' })
@@ -13,8 +13,8 @@ router
       })
       .post('/', (req, res) => {
             db.add(req.body)
-                  .then(dish => {
-                        res.status(201).json({ message: 'new dish created'})
+                  .then(ingredient => {
+                        res.status(201).json({ message: 'new ingredient created'})
                   })
                   .catch(err => {
                         console.log(err);
@@ -26,12 +26,12 @@ router
       .get('/:id', (req, res) => {
             const id = req.params.id;
             db.findById(id)
-                  .then(dish => {
-                  res.status(200).json(dish);
+                  .then(ingredient => {
+                  res.status(200).json(ingredient);
                   })
                   .catch(err => {
                         console.log(err);
-                        res.status(500).json({ message: "Couldn't get that dish.." });
+                        res.status(500).json({ message: "Couldn't get that ingredient.." });
                   });
       })
 
